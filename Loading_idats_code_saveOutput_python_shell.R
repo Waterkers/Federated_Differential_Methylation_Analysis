5b4388d4cad8c087dd1ecad58ae6adc361e796a0
Silke <silke.vanelferen@gmail.com>
2022-04-25 22:16:52 +0200

    changed the code that created the file structure to allow for an identifier

--- Loading_idats_code_saveOutput_python_shell.R
+++ Loading_idats_code_saveOutput_python_shell.R
@@ -5,6 +5,7 @@ input <- commandArgs(trailingOnly = TRUE)
 idat <- input[1]
 pheno_information <- input[2]
 working_dir <- input[3]
+identifier <- input[4]
 
 ##### Start with installing the required Bioconductor and then packages ########
 if (!require("BiocManager", quietly = TRUE))
@@ -19,19 +20,19 @@ library(wateRmelon)
 
 
 ##### source the Exeter functions needed for the pipeline
-lapply(list.files("E:\\Msc Systems Biology\\MSB5000_Master_Thesis\\ExeterEWASPipeline-master\\R",pattern = "\\.r$",full.names = T),function(x){source(x)})
+lapply(list.files("E:\\Msc Systems Biology\\MSB5000_Master_Thesis\\Practical work\\Federated_Differential_Methylation_Analysis\\Required_files",pattern = "\\.r$",full.names = T),function(x){source(x)})
 
 ## Set the working directory
 setwd(working_dir) # set working directory
 
 # create the output directory
-if(!dir.exists("QC_GSE66351_PythonShell")){
-  dir.create("QC_GSE66351_PythonShell")
+QC_output <- paste0("QC_", identifier)
+if(!dir.exists(QC_output)){
+	dir.create(QC_output)
 }
-if(!dir.exists("QC_GSE66351_PythonShell\\Plots")){
-  dir.create("QC_GSE66351_PythonShell\\Plots")
-}
-
+if(!dir.exists(file.path(QC_output, "Plots"))){
+  dir.create(file.path(QC_output, "Plots"))
+} 
 print("Output directories created")
 
 #Creating a function around the code to load the data
@@ -43,35 +44,32 @@ pheno1 <- as.data.frame(pheno1)
 colnames(pheno1)<- pheno1[1,]
 pheno1 <- pheno1[2:191,]
 
-write.csv(pheno1, "QC_GSE66351_PythonShell\\pheno_check.csv")
 print("Phenotype information imported")
 
 Sample_ID <- getBarcodes(idat_path)
-write.csv(Sample_ID, "QC_GSE66351_PythonShell\\barcodes_check1.csv")
 pheno1 <- cbind(pheno1, Sample_ID)
+
 pheno1_half <- as.data.frame(pheno1[1:20,])
-
-write.csv(pheno1, "QC_GSE66351_PythonShell\\input_check.csv")
-
 barcodes_GSE66351_half <- pheno1_half$Sample_ID # my personal laptop cannot deal with all 190 samples so I'm trying it with the first 20 instead
-write.csv(barcodes_GSE66351_half, "QC_GSE66351_PythonShell\\barcodes_check2.csv")
+
 data <- wateRmelon::readEPIC(barcodes = barcodes_GSE66351_half, pdat = pheno1_half, idatPath = idat_path)
 
-save(data, file = "QC_GSE66351_PythonShell\\methylumiSet.RData")
-write.csv(pheno1,"QC_GSE66351_PythonShell\\input_check_postIDAT.csv")
-print("idats")
+save(data, pheno1, file = file.path(QC_output, "methylumiSet_PhenoDatafrma.RData"))
+print("Finished reading and saving .idat files")
 
 #save the information stored in the data2 object (the MethylumiSet object thing) into seperate dataframes
 setwd(working_dir)
+
 # betas
 raw_betas <- betas(data)
-write.csv(betas, "QC_GSE66351_PythonShell\\Raw_betas.csv")
+write.csv(betas, file.path(QC_output, "Raw_betas.csv"))
 # methylated values
 raw_methylated <- methylumi::methylated(data)
-write.csv(raw_methylated, "QC_GSE66351_PythonShell\\Raw_methylated_intensities.csv")
+write.csv(raw_methylated, file.path(QC_output, "Raw_methylated_intensities.csv"))
 # unmethylated values
 raw_unmethylated <- methylumi::unmethylated(data)
-write.csv(raw_unmethylated, "QC_GSE66351_PythonShell\\Raw_unmethylated_intensities.csv")
+write.csv(raw_unmethylated, file.path(QC_output, "Raw_unmethylated_intensities.csv"))
+
 
 }
 
