efc1f27dd407524e8261107527601d2935e0c74a
Silke van Elferen <elfiezelfie@gmail.com>
2025-03-26 17:31:59 +0100

    parallel with telegram bot progress tracker

--- Python_translation/EWAS_central_Parallel.py
+++ Python_translation/EWAS_central_Parallel.py
@@ -2,11 +2,19 @@ from pyexpat.errors import XML_ERROR_FEATURE_REQUIRES_XML_DTD
 import numpy as np
 import pandas as pd
 import scipy.stats
+import yaml
 from statsmodels.stats.multitest import multipletests
 from tqdm.contrib.concurrent import process_map
+from tqdm.contrib.telegram import tqdm
 import multiprocessing
 import os
 from functools import partial
+import argparse
+
+
+config_file = '/home/silke/Documents/Fed_EWAS/telegrambot_config.yml'
+with open(config_file) as configStream:
+    config = yaml.load(configStream, Loader=yaml.Loader)
 
 def EWASCalculation(row, x_matrix):
     """
@@ -33,7 +41,7 @@ def EWASCalculation(row, x_matrix):
     df = row.shape[0] - 2  # degrees of freedom is defined as number of observations - 2
     p = scipy.stats.t.sf(abs(t), df)
     #p_value.append(p)
-    return coef, stan_er, m, stersq, t, p
+    return coef, stan_er, m, stersq, t, p#{'coef':coef, 'stan_er':stan_er, 'm':m, 'stersq':stersq, 't':t, 'p':p}
 
 
 
@@ -45,34 +53,40 @@ def EWAS_central(design_matrix, beta_values):
     n = y_matrix.shape[0] # select the number of rows of the beta matrix - #genes that the linear model will be calculated for
     m = x_matrix.shape[1] #select the number of columns from the design matrix
 
+    coefficient = []
+    standard_error = []
+    mu = []
+    SSE = []
+    t_stat = []
+    p_value = []
     corrected_pvalue = []
+    with multiprocessing.Pool(processes=max(int(os.cpu_count() / 2), 2)) as processPool, tqdm(total=int(n),
+                                                                                              token=
+                                                                                              config[
+                                                                                                  'serverProgressBot'][
+                                                                                                  'token'],
+                                                                                              chat_id=
+                                                                                              config[
+                                                                                                  'serverProgressBot'][
+                                                                                                  'chat_id']) as pbar:
+        EWASCalculationPartial = partial(EWASCalculation, x_matrix=x_matrix)
+        for result in processPool.imap(EWASCalculationPartial, y_matrix):
+            # increment the progress bar
+            pbar.update()
+            pbar.refresh()
+            print(len(result))
+            coef, stan_er, m, stersq, t, p = result
+            coefficient.append(coef)
+            standard_error.append(stan_er)
+            mu.append(m)
+            SSE.append(stersq)
+            t_stat.append(t)
+            p_value.append(p)
 
-    # with multiprocessing.Pool(processes=max(os.cpu_count() - 2, 2)) as processPool:
-    #     EWASCalculationPartial = partial(EWASCalculation, x_matrix=x_matrix)
-    #     coefficient, standard_error, mu, SSE, t_stat, p_value = zip(*processPool.map(EWASCalculationPartial, y_matrix))
-    EWASCalculationPartial = partial(EWASCalculation, x_matrix=x_matrix)
-    coefficient, standard_error, mu, SSE, t_stat, p_value = zip(*process_map(EWASCalculationPartial, y_matrix))
 
-    # for i in tqdm(range(0, n), token='7885595681:AAFmqfAkTqvAdOCRWF1RMl0s26XUmj5_Yv8', chat_id='5671488828'):
-    #     y_m = y_matrix[i, :]
-    #     x_t = x_matrix.T @ x_matrix
-    #     x_t_y = x_matrix.T @ y_m
-    #     x_t_inv = np.linalg.inv(x_t)
-    #     coef = x_t_inv @ x_t_y #beta
-    #     coefficient.append(coef)
-    #     stan_er = np.sqrt(np.diag(x_t_inv)) #stdev_unscaled
-    #     standard_error.append(stan_er)
-    #     m = x_matrix @ coef
-    #     mu.append(m)
-    #     stersq = np.sum((y_m - mu)**2)
-    #     SSE.append(stersq)
-    #
-    #     t = coef/stan_er
-    #     t_stat.append(t)
-    #     df = y_matrix.shape[1]-2 #degrees of freedom is defined as number of observations - 2
-    #     p = scipy.stats.t.sf(abs(t), df)
-    #     p_value.append(p)
-    #
+    # EWASCalculationPartial = partial(EWASCalculation, x_matrix=x_matrix)
+    # coefficient, standard_error, mu, SSE, t_stat, p_value = zip(*process_map(EWASCalculationPartial, y_matrix, max_workers=max(int(os.cpu_count() / 2), 2)))
+
     #turn the results saved in lists into a dataframe for each covariate with the probe ids as index
     for i in range(0,len(p_value)):
         corrected_pvalue.append(multipletests(p_value[i], method="fdr_bh")[1])
@@ -86,4 +100,11 @@ def EWAS_central(design_matrix, beta_values):
     return results_EWAS, SSE
 
 if __name__ == '__main__':
-    EWAS_central()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('designMatrix', help='design matrix')
+    # parser.add_argument('betaValues', help='beta values')
+    # EWAS_central(parser.parse_args().designMatrix, parser.parse_args().betaValues)
+    designMatrix = pd.read_csv('/home/silke/Documents/Fed_EWAS/Data/GSE134379_RAW/Small_EWAS_design.csv', index_col=0)
+    normalisedBetas = pd.read_csv(
+        '/home/silke/Documents/Fed_EWAS/Data/QC_Python/GSE134379_half_normalised_betas_python.csv', index_col=0)
+    results_EWAS, SSE = EWAS_central(designMatrix, normalisedBetas.iloc[0:1000,:])
